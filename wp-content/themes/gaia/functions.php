612598ad1a34a412f7bf52424e9e30b2ff6ad030
Rizal Chris <rizalchrisdev@gmail.com>
2026-01-22 17:37:58 +0800

    WordPress Starter

---
+++ wp-content/themes/gaia/functions.php
@@ -0,0 +1,43 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function enqueue_jquery_script() {
+	wp_enqueue_script( 'jquery' );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_jquery_script' );
+
+add_theme_support( 'post-thumbnails' );
+remove_theme_support( 'widgets-block-editor' );
+
+add_filter( 'use_block_editor_for_post', '__return_false' );
+
+
+register_nav_menus( [
+	'primary' => 'Primary Menu',
+] );
+
+function enqueue_assets() {
+	if ( is_admin() ) {
+		return;
+	}
+
+	// Main CSS
+	wp_enqueue_style(
+		'main',
+		get_template_directory_uri() . '/assets/css/main.css',
+		array(),
+		filemtime( get_template_directory() . '/assets/css/main.css' )
+	);
+
+	// Bootstrap
+	wp_enqueue_style( 'bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' );
+	wp_enqueue_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', null, null, true );
+	wp_enqueue_style( 'swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css' );
+	wp_enqueue_script( 'swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', null, null, true );
+	wp_enqueue_style( 'fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'enqueue_assets' );
---
+++ wp-content/themes/gaia/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+get_header();
+the_content();
+get_footer();
+
+?>
