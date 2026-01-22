3f604a79b1edbebba83dc62aca6c68ad856e4adf
Rizal Chris <rizalchrisdev@gmail.com>
2026-01-23 00:50:58 +0800

    [Add] Must Use Plugin: Disable Gutenberg

--- wp-content/mu-plugins/secure-custom-fields.php
+++ wp-content/mu-plugins/secure-custom-fields.php
@@ -8,5 +8,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+//ACF to JSON
+function acf_json( $paths ) {
+	$paths[] = WP_CONTENT_DIR . '/mu-plugins/secure-custom-fields/acf-json';
+	return $paths;
+}
+add_filter( 'acf/settings/load_json', 'acf_json' );
+
+
 // Load Secure Custom Fields
 require_once WP_CONTENT_DIR . '/mu-plugins/secure-custom-fields/acf.php';
--- wp-content/themes/gaia/functions.php
+++ wp-content/themes/gaia/functions.php
@@ -4,16 +4,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_theme_support( 'woocommerce' );
-add_theme_support( 'post-thumbnails' );
+// add_theme_support( 'woocommerce' );
+// add_theme_support( 'post-thumbnails' );
+add_filter( 'use_block_editor_for_post', '__return_false', 100 );
+add_filter( 'use_block_editor_for_post_type', '__return_false', 100 );
 remove_theme_support( 'widgets-block-editor' );
-add_filter( 'use_block_editor_for_post', '__return_false' );
 
 
 register_nav_menus( [
 	'primary' => 'Primary Menu',
 ] );
 
+// Enqueue jQuery
 function enqueue_jquery_script() {
 	wp_enqueue_script( 'jquery' );
 }
@@ -34,9 +36,9 @@ function enqueue_assets() {
 
 	// Bootstrap
 	wp_enqueue_style( 'bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' );
-	wp_enqueue_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', null, null, true );
+	wp_enqueue_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array( 'jquery' ), null, true );
 	wp_enqueue_style( 'swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css' );
-	wp_enqueue_script( 'swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', null, null, true );
+	wp_enqueue_script( 'swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array( 'jquery' ), null, true );
 	wp_enqueue_style( 'fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css' );
 }
 
